4bcfd54efbe8854cb6f14f86b69a5acd68679ffd
Oscar Olsson <happybits@gmail.com>
2023-04-04 16:24:31 +0200

    -

---
+++ Property/Ex01/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpCourse.Property.Ex01;
+
+public class Solution
+{
+    public static void Run()
+    {
+        Address address = new Address
+        {
+            Street = "Långa gatan",
+            StreetNumber = "13B",
+            City = "Sundsvall",
+            ZipCode = "111 22"
+        };
+
+        Console.WriteLine("Street".PadRight(30) + address.Street);
+        Console.WriteLine("StreetNumber".PadRight(30) + address.StreetNumber);
+        Console.WriteLine("City".PadRight(30) + address.City);
+        Console.WriteLine("ZipCode".PadRight(30) + address.ZipCode);
+        Console.WriteLine("FullStreet".PadRight(30) + address.FullStreet);
+    }
+}
---
+++ Property/Ex02/Solution.cs
@@ -0,0 +1,68 @@
+﻿using ConsoleUtilities;
+
+namespace CSharpCourse.Property.Ex02;
+
+public class Solution
+{
+    public static void Run()
+    {
+        void SetWithProperty()
+        {
+            ConsoleCompanion cc = new ConsoleCompanion();
+
+            Address address = new Address
+            {
+                Street = "Långa gatan",
+                StreetNumber = "13B",
+                City = "Sundsvall",
+            };
+
+            cc.WriteLineDark("Try to set zipcode to 111 22");
+            address.ZipCode = "111 22";
+            cc.Columns("ZipCode", address.ZipCode);
+            Console.WriteLine();
+
+            cc.WriteLineDark("Try to set zipcode to 222 33");
+            address.ZipCode = "222 33";
+            cc.Columns("ZipCode", address.ZipCode);
+            Console.WriteLine();
+
+            cc.WriteLineDark("Try to set zipcode to HELLO");
+            address.ZipCode = "HELLO";
+            cc.Columns("ZipCode", address.ZipCode);
+
+            // 
+        }
+
+        void SetWithMethods()
+        {
+            ConsoleCompanion cc = new ConsoleCompanion();
+
+            Address address = new Address
+            {
+                Street = "Långa gatan",
+                StreetNumber = "13B",
+                City = "Sundsvall",
+            };
+
+
+            cc.WriteLineDark("Try to set zippycode to 111 22");
+            address.SetZipCode("111 22");
+            cc.Columns("ZippyCode", address.GetZipCode());
+            Console.WriteLine();
+
+            cc.WriteLineDark("Try to set zippycode to 222 33");
+            address.SetZipCode("222 33");
+            cc.Columns("ZippyCode", address.GetZipCode());
+            Console.WriteLine();
+
+            cc.WriteLineDark("Try to set zippycode to HELLO");
+            address.SetZipCode("HELLO");
+            cc.Columns("ZippyCode", address.GetZipCode());
+        }
+
+
+        SetWithProperty();
+
+    }
+}
