92390b7d73fb08f6f7595f4c5381f9b0906c53c9
Oscar Olsson <happybits@gmail.com>
2023-04-05 15:51:39 +0200

    -

---
+++ AbstractClasses/Ex01/ChangeSwedishLettersFileTransformer.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpCourse.AbstractClasses.Ex01;
+
+class ChangeSwedishLettersFileTransformer : FileTransformer
+{
+    protected override string Transform(string content)
+    {
+        return content
+            .Replace("å", "a")
+            .Replace("Å", "A")
+            .Replace("ä", "a")
+            .Replace("Ä", "A")
+            .Replace("ö", "o")
+            .Replace("Ö", "O");
+    }
+}
---
+++ AbstractClasses/Ex01/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpCourse.AbstractClasses.Ex01;
+
+public class Solution
+{
+    public static void Run()
+    {
+        //var t0 = new FileTransformer(); // Kan inte skapa instans av abstrakt klass
+
+        string dataFolder = @"C:\Project\CSharpCourse\CSharpCourse\AbstractClasses\Ex01\data\";
+
+        var t1 = new UpperCaseFileTransformer();
+        t1.TransformFile(Path.Combine(dataFolder, "sample1.txt"));
+
+        var t2 = new RemoveStarsFileTransformer();
+        t2.TransformFile(Path.Combine(dataFolder, "sample2.txt"));
+
+        var t3 = new ChangeSwedishLettersFileTransformer();
+        t3.TransformFile(Path.Combine(dataFolder, "sample3.txt"));
+    }
+}
